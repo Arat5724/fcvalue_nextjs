b9e0e4374f9183b5d1cb770c0dea013f5aaa5b11
Arat5724 <arat5724@gmail.com>
2023-12-18 18:27:04 +0900

    Updates

--- _next/static/chunks/app/page-ab3a1250a3f7a8db.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{9117:function(n,e,t){Promise.resolve().then(t.t.bind(t,5250,23)),Promise.resolve().then(t.t.bind(t,4220,23))},4220:function(n){n.exports={"link-button":"page_link-button__7wZ83"}}},function(n){n.O(0,[250,971,938,744],function(){return n(n.s=9117)}),_N_E=n.O()}]);
