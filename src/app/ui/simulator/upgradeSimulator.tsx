2907edad6377ecc1f1f8333b6c7f3e73b15967d7
Arat5724 <arat5724@gmail.com>
2023-12-31 19:04:07 +0900

    feat: upgrade simulator ovr

--- src/app/ui/simulator/upgradeSimulator.tsx
+++ src/app/ui/simulator/upgradeSimulator.tsx
@@ -242,9 +242,12 @@ function UpgradeSimulator({ player }: {
 }) {
   const [upgrade, setUpgrade] = useState<number>(1);
   const [blockState, setBlockState] = useState<number>(5);
-  const [successNum, setSuccessNum] = useState<number[][]>((new Array<number[]>(10)).fill(new Array<number>(51).fill(0)));
-  const [failureNum, setFailureNum] = useState<number[][]>((new Array<number[]>(10)).fill(new Array<number>(51).fill(0)));
+  const [eventBlockState, setEventBlockState] = useState<number>(0);
+
+  const [successNum, setSuccessNum] = useState<number>(0);
+  const [failureNum, setFailureNum] = useState<number>(0);
   const [history, setHistory] = useState<UpgradeHistory[]>([]);
+  const [isOption1, setIsOption1] = useState<boolean>(true);
 
   const [result, setUpgradeResult] = useState<UpgradeResult>(UpgradeResult.No);
   const [isSelectorOpen, setIsSelectorOpen] = useState<boolean>(false);
@@ -254,22 +257,19 @@ function UpgradeSimulator({ player }: {
   function upgradePlayer() {
     setUpgradeResult(UpgradeResult.Upgrading);
     setIsSelectorOpen(false);
-    const prob = upgradeProb[upgrade] * blockState / 5;
+    const prob = upgradeProb[upgrade] * (blockState + eventBlockState) / 5;
     if (Math.random() < prob) {
       setTimeoutId(setTimeout(() => {
         setUpgradeResult(UpgradeResult.Success);
         setLocalUpgrade(localUpgrade + 1);
-        const newSuccessNum = [[...successNum[0]], [...successNum[1]], [...successNum[2]], [...successNum[3]], [...successNum[4]],
-        [...successNum[5]], [...successNum[6]], [...successNum[7]], [...successNum[8]], [...successNum[9]]];
-        newSuccessNum[upgrade][blockState * 10]++;
-        setSuccessNum(newSuccessNum);
+        setSuccessNum(successNum + 1);
         setHistory([{
           season: player?.season ?? "",
           name: player?.name ?? "",
           before: localUpgrade,
           after: localUpgrade + 1,
           probablity: prob,
-          blockState: blockState
+          blockState: blockState + eventBlockState
         }, ...history.slice(0, 99)]);
       }, 2000))
     } else {
@@ -279,17 +279,14 @@ function UpgradeSimulator({ player }: {
       setTimeoutId(setTimeout(() => {
         setUpgradeResult(UpgradeResult.Failure);
         setLocalUpgrade(afterUpgrade);
-        const newFailureNum = [[...failureNum[0]], [...failureNum[1]], [...failureNum[2]], [...failureNum[3]], [...failureNum[4]],
-        [...failureNum[5]], [...failureNum[6]], [...failureNum[7]], [...failureNum[8]], [...failureNum[9]]];
-        newFailureNum[upgrade][blockState * 10]++;
-        setFailureNum(newFailureNum);
+        setFailureNum(failureNum + 1);
         setHistory([{
           season: player?.season ?? "",
           name: player?.name ?? "",
           before: localUpgrade,
           after: afterUpgrade,
           probablity: prob,
-          blockState: blockState
+          blockState: blockState + eventBlockState
         }, ...history.slice(0, 99)]);
       }, 2000))
     }
@@ -311,6 +308,8 @@ function UpgradeSimulator({ player }: {
 
   return <>
     <div className={styles["simulator-section"]}>
+      <SwitchButton state={isOption1} setState={(e) => { setIsOption1(e); setBlockState(5); setEventBlockState(0); resetPlayer(); }} />
+
       <div className={styles["simulator-section__result"]}>
         <span>
           {player === undefined ? "선수를 선택하세요"
@@ -365,35 +364,239 @@ function UpgradeSimulator({ player }: {
       <div className={styles["simulator-section__information"]}>
         <p>
           <span>강화 확률 </span>
-          <span className={styles.primary}>{parseFloat((upgradeProb[upgrade] * blockState * 20).toFixed(2))}</span>
+          <span className={styles.primary}>{parseFloat((upgradeProb[upgrade] * (blockState + eventBlockState) * 20).toFixed(2))}</span>
           <span>%</span>
         </p>
         <p>
-          <span className={styles.primary}>{blockState}</span>
+          <span className={styles.primary}>{parseFloat((blockState + eventBlockState).toFixed(5))}</span>
           <span> 칸</span>
         </p>
       </div>
-      <BlockBar result={result} blockState={blockState} setBlockState={setBlockState} />
       {
-        player === undefined ? <button className={styles["button__upgrade"]} disabled={true}>선수를 선택하세요</button>
-          : result === UpgradeResult.No ? <button className={styles["button__upgrade"]} onClick={upgradePlayer}>강화 시도</button>
-            : <button className={styles["button__retry"]} onClick={resetPlayer}>다시 시도</button>
+        isOption1 ? <BlockBar result={result} blockState={blockState} setBlockState={setBlockState} />
+          : <OVRInput result={result} blockState={blockState} setBlockState={setBlockState}
+            eventBlockState={eventBlockState} setEventBlockState={setEventBlockState} ovr={(player?.ovr ?? 0) + upgradeOvr[upgrade]} upgrade={upgrade} />
       }
-      <div className={styles["result-table"]}>
-        <table >
-          <thead><tr><th>시도</th><th>성공</th><th>실패</th><th>성공 비율</th></tr></thead>
-          <tbody><tr>
-            <td>{successNum[upgrade][blockState * 10] + failureNum[upgrade][blockState * 10]}</td>
-            <td>{successNum[upgrade][blockState * 10]}</td>
-            <td>{failureNum[upgrade][blockState * 10]}</td>
-            <td>{successNum[upgrade][blockState * 10] === 0 ? 0
-              : (successNum[upgrade][blockState * 10] / (successNum[upgrade][blockState * 10] + failureNum[upgrade][blockState * 10]) * 100).toFixed(2)}%</td>
-          </tr></tbody>
-        </table>
+      <div>
+        {
+          player === undefined ? <button className={styles["button__upgrade"]} disabled={true}>선수를 선택하세요</button>
+            : result === UpgradeResult.No ? <button className={styles["button__upgrade"]} onClick={upgradePlayer}>강화 시도</button>
+              : <button className={styles["button__retry"]} onClick={resetPlayer}>다시 시도</button>
+        }
       </div>
+      <UpgradeResultTable successNum={successNum} failureNum={failureNum} reset={() => { setSuccessNum(0); setFailureNum(0); }} result={result} />
     </div>
     <h2>강화 히스토리</h2>
     <AdInArticle />
+    <UpgradeHistoryTable history={history} />
+  </>
+}
+
+const gages = [
+  [[2.5, 1.865, 1.395, 1.04, 0.78, 0.58, 0.435, 0.325, 0.245, 0.185, 0.135, 0.105, 0.075, 0.06, 0.045, 0.03, 0.025, 0.02, 0.015, 0.01, 0.01, 0.005, 0.005, 0.005, 0.005],
+  [2.5, 3.35, 4.49]],
+  [[1.665, 1.245, 0.93, 0.695, 0.52, 0.385, 0.29, 0.215, 0.16, 0.12, 0.09, 0.07, 0.05, 0.04, 0.03, 0.02, 0.015, 0.01, 0.01, 0.005, 0.005, 0.005, 0.005],
+  [1.665, 2.235, 2.995, 4.02]],
+  [[1.25, 0.93, 0.695, 0.52, 0.39, 0.29, 0.215, 0.16, 0.12, 0.09, 0.07, 0.05, 0.04, 0.03, 0.02, 0.015, 0.01, 0.01, 0.005, 0.005, 0.005, 0.005],
+  [1.25, 1.675, 2.25, 3.02, 4.05]],
+  [[1.0, 0.745, 0.555, 0.415, 0.31, 0.23, 0.17, 0.13, 0.095, 0.07, 0.055, 0.04, 0.03, 0.025, 0.015, 0.015, 0.01, 0.005, 0.005, 0.005, 0.005],
+  [1.0, 1.34, 1.8, 2.42, 3.25, 4.37]],
+  [[1.0, 0.745, 0.555, 0.415, 0.31, 0.23, 0.17, 0.13, 0.095, 0.07, 0.055, 0.04, 0.03, 0.02, 0.015, 0.015, 0.01, 0.005, 0.005, 0.005, 0.005],
+  [1.0, 1.345, 1.805, 2.425, 3.26, 4.385]],
+  [[1.0, 0.745, 0.555, 0.415, 0.31, 0.23, 0.17, 0.13, 0.095, 0.07, 0.055, 0.04, 0.03, 0.02, 0.015, 0.01, 0.01, 0.005, 0.005, 0.005, 0.005],
+  [1.0, 1.345, 1.805, 2.43, 3.27, 4.4]],
+  [[1.0, 0.745, 0.555, 0.41, 0.305, 0.23, 0.17, 0.125, 0.095, 0.07, 0.05, 0.04, 0.03, 0.02, 0.015, 0.01, 0.01, 0.005, 0.005, 0.005, 0.005],
+  [1.0, 1.345, 1.81, 2.44, 3.285, 4.425]],
+  [[1.0, 0.74, 0.55, 0.41, 0.305, 0.225, 0.17, 0.125, 0.095, 0.07, 0.05, 0.04, 0.03, 0.02, 0.015, 0.01, 0.01, 0.005, 0.005, 0.005, 0.005],
+  [1.0, 1.345, 1.815, 2.45, 3.305, 4.455]],
+  [[1.0, 0.74, 0.55, 0.41, 0.305, 0.225, 0.165, 0.125, 0.09, 0.07, 0.05, 0.04, 0.03, 0.02, 0.015, 0.01, 0.01, 0.005, 0.005, 0.005, 0.005],
+  [1.0, 1.35, 1.82, 2.46, 3.32, 4.485]],
+];
+
+function getGage(ovr: number, upgrade: number, inputOvr: number) {
+  const gage = gages[upgrade - 1][inputOvr - ovr > 0 ? 1 : 0];
+  const dist = Math.abs(inputOvr - ovr);
+  if (dist >= gage.length) return inputOvr - ovr > 0 ? 5 : 0;
+  return gage[dist];
+}
+
+function OVRInput({ result, blockState, setBlockState, eventBlockState, setEventBlockState, ovr, upgrade }: {
+  result: UpgradeResult,
+  blockState: number, setBlockState: (blockState: number) => void,
+  eventBlockState: number, setEventBlockState: (eventBlockState: number) => void,
+  ovr: number, upgrade: number
+}) {
+  const [ovrState, setOvrState] = useState<number[]>([ovr, ovr, ovr, ovr, ovr]);
+  const [isClosed, setIsClosed] = useState<boolean>(true);
+  const [upgradeEvent, setUpgradeEvent] = useState<number>(0);
+
+  const onInput = (index: number) => (e: React.FormEvent<HTMLInputElement>) => {
+    const newOvrState = [...ovrState];
+    const newValue = e.currentTarget.value === "" ? 0 : parseInt(e.currentTarget.value);
+    newOvrState[index] = Math.min(newValue, 200);
+    setOvrState(newOvrState);
+  }
+
+  const add1 = (index: number) => () => {
+    const newOvrState = [...ovrState];
+    newOvrState[index] = Math.min(newOvrState[index] + 1, 200);
+    setOvrState(newOvrState);
+  }
+
+  const sub1 = (index: number) => () => {
+    const newOvrState = [...ovrState];
+    newOvrState[index] = Math.max(newOvrState[index] - 1, 0);
+    setOvrState(newOvrState);
+  }
+
+  const initAll = () => {
+    setOvrState([ovr, ovr, ovr, ovr, ovr]);
+  }
+
+  const add1All = () => {
+    const newOvrState = [...ovrState];
+    for (let i = 0; i < 5; i++) newOvrState[i] = Math.min(newOvrState[i] + 1, 200);
+    setOvrState(newOvrState);
+  }
+
+  const sub1All = () => {
+    const newOvrState = [...ovrState];
+    for (let i = 0; i < 5; i++) newOvrState[i] = Math.max(newOvrState[i] - 1, 0);
+    setOvrState(newOvrState);
+  }
+
+  useEffect(() => {
+    let newBlockState = 0;
+    for (const inputOvr of ovrState)
+      newBlockState += getGage(ovr, upgrade, Number.isNaN(inputOvr) ? 0 : inputOvr);
+    newBlockState = Math.min(newBlockState, 5);
+    setBlockState(newBlockState);
+    const newEventBlockState = Math.min(upgradeEvent / 100 * newBlockState, 5 - newBlockState);
+    setEventBlockState(newEventBlockState);
+  }, [ovrState, ovr, upgrade, upgradeEvent]);
+
+  useEffect(() => {
+    if (result !== UpgradeResult.No) setIsClosed(true);
+  }, [result]);
+
+  return <>
+    <div className={styles["block-bar__info2"]}>
+      <div className={styles["block-bar"]}>
+        <div className={styles.gage__default}></div>
+        <div className={styles.gage__event}
+          style={{ width: `${(blockState + eventBlockState) * 20}%` }}></div>
+        <div className={styles.gage__input}
+          style={{ width: `${blockState * 20}%` }}></div>
+      </div>
+    </div>
+    <div className={styles["ovr-input"]}>
+      <div className={styles["ovr-input__warp"]}>
+        <button onClick={add1(0)} disabled={result !== UpgradeResult.No}>+1</button>
+        <input type="number" min="0" max="200" step="1" value={ovrState[0]} onInput={onInput(0)} disabled={result !== UpgradeResult.No}></input>
+        <button onClick={sub1(0)} disabled={result !== UpgradeResult.No}>-1</button>
+      </div>
+      <div className={styles["ovr-input__warp"]}>
+        <button onClick={add1(1)} disabled={result !== UpgradeResult.No}>+1</button>
+        <input type="number" min="0" max="200" step="1" value={ovrState[1]} onInput={onInput(1)} disabled={result !== UpgradeResult.No}></input>
+        <button onClick={sub1(1)} disabled={result !== UpgradeResult.No}>-1</button>
+      </div>
+      <div className={styles["ovr-input__warp"]}>
+        <button onClick={add1(2)} disabled={result !== UpgradeResult.No}>+1</button>
+        <input type="number" min="0" max="200" step="1" value={ovrState[2]} onInput={onInput(2)} disabled={result !== UpgradeResult.No}></input>
+        <button onClick={sub1(2)} disabled={result !== UpgradeResult.No}>-1</button>
+      </div>
+      <div className={styles["ovr-input__warp"]}>
+        <button onClick={add1(3)} disabled={result !== UpgradeResult.No}>+1</button>
+        <input type="number" min="0" max="200" step="1" value={ovrState[3]} onInput={onInput(3)} disabled={result !== UpgradeResult.No}></input>
+        <button onClick={sub1(3)} disabled={result !== UpgradeResult.No}>-1</button>
+      </div>
+      <div className={styles["ovr-input__warp"]}>
+        <button onClick={add1(4)} disabled={result !== UpgradeResult.No}>+1</button>
+        <input type="number" min="0" max="200" step="1" value={ovrState[4]} onInput={onInput(4)} disabled={result !== UpgradeResult.No}></input>
+        <button onClick={sub1(4)} disabled={result !== UpgradeResult.No}>-1</button>
+      </div>
+    </div>
+    <div className={styles["ovr-input__wrap--all"]}>
+      <button onClick={initAll} disabled={result !== UpgradeResult.No}>전체 {ovr}로 초기화</button>
+      <button onClick={add1All} disabled={result !== UpgradeResult.No}>전체 +1</button>
+      <button onClick={sub1All} disabled={result !== UpgradeResult.No}>전체 -1</button>
+    </div>
+    <div className={styles["upgrade-event__wrap"]}>
+      <div className={styles["upgrade-event__text"]}>강화 부스트 이벤트 적용</div>
+      <div className={styles["upgrade-event"]}>
+        <button
+          onClick={() => setIsClosed(!isClosed)}
+          className={clsx(styles["upgrade-event__button"], styles["icon-" + (isClosed ? "closed" : "open")])}
+          disabled={result !== UpgradeResult.No}
+        >
+          {upgradeEvent === 0 ? "미적용" : `${upgradeEvent}% 혜택`}
+        </button>
+        <ul hidden={isClosed} className={styles["upgrade-event__list"]}>
+          <li onClick={() => { setIsClosed(true); setUpgradeEvent(0); }}>미적용</li>
+          <li onClick={() => { setIsClosed(true); setUpgradeEvent(35); }}>35% 혜택</li>
+          <li onClick={() => { setIsClosed(true); setUpgradeEvent(30); }}>30% 혜택</li>
+          <li onClick={() => { setIsClosed(true); setUpgradeEvent(25); }}>25% 혜택</li>
+        </ul>
+      </div>
+    </div>
+  </>;
+}
+
+function BlockBar({ result, blockState, setBlockState }:
+  { result: UpgradeResult, blockState: number, setBlockState: (blockState: number) => void }) {
+  return <div className={styles["block-bar__info1"]}>
+    <div className={clsx(styles["block-bar"],)}>
+      <div className={styles.gage__default}></div>
+      <div
+        className={styles.gage__input}
+        style={{ width: `${blockState * 20}%` }}></div>
+      <input
+        type="range" min="0.1" max="5" step="0.1"
+        value={blockState}
+        disabled={result !== UpgradeResult.No}
+        onInput={(e) => setBlockState(parseFloat(e.currentTarget.value))}></input>
+    </div>
+  </div>
+}
+
+
+export function SwitchButton({ state, setState }: {
+  state: boolean, setState: (state: boolean) => void
+}) {
+  return <div onClick={() => setState(!state)} className={styles["switch__wrap"]}>
+    <div>
+      게이지 조절
+    </div>
+    <div className={styles["switch"]}>
+      <span className={clsx(styles["switch__button"], styles[state ? "left" : "right"])}></span>
+    </div>
+    <div>
+      능력치 입력
+    </div>
+  </div>;
+}
+
+function UpgradeResultTable({ successNum, failureNum, reset, result }: {
+  successNum: number, failureNum: number, reset: () => void, result: UpgradeResult
+}) {
+  return <div className={styles["result-table"]}>
+    <table>
+      <thead><tr><th>시도</th><th>성공</th><th>실패</th><th>성공 비율</th></tr></thead>
+      <tbody><tr>
+        <td>{successNum + failureNum}</td>
+        <td>{successNum}</td>
+        <td>{failureNum}</td>
+        <td>{successNum === 0 ? 0 : (successNum / (successNum + failureNum) * 100).toFixed(2)}%</td>
+      </tr></tbody>
+    </table>
+    <button onClick={reset} disabled={result === UpgradeResult.Upgrading} className={styles["result-table__button"]}>
+      초기화
+    </button>
+  </div>
+}
+
+function UpgradeHistoryTable({ history }: { history: UpgradeHistory[] }) {
+  return <>
     <div className={styles["upgrade-history"]}>
       <table>
         <thead><tr><th>선수</th><th>시작</th><th>결과</th><th>강화 확률</th><th>칸</th></tr></thead>
@@ -404,7 +607,7 @@ function UpgradeSimulator({ player }: {
             <td><SelectorItemDiv grade={h.before} /></td>
             <td><SelectorItemDiv grade={h.after} /></td>
             <td>{parseFloat((h.probablity * 100).toFixed(2))}%</td>
-            <td>{h.blockState}</td>
+            <td>{parseFloat((h.blockState).toFixed(5))}</td>
           </tr>)}
         </tbody>
       </table>
@@ -413,18 +616,10 @@ function UpgradeSimulator({ player }: {
   </>
 }
 
-function BlockBar({ result, blockState, setBlockState }:
-  { result: UpgradeResult, blockState: number, setBlockState: (blockState: number) => void }) {
-  return <div className={styles["block-bar"]}>
-    <div className={styles.gage__default}></div>
-    <div
-      className={styles.gage__input}
-      style={{ width: `${blockState * 20}%` }}></div>
-    <input
-      type="range" min="0.1" max="5" step="0.1"
-      value={blockState}
-      disabled={result !== UpgradeResult.No}
-      onInput={(e) => setBlockState(parseFloat(e.currentTarget.value))}></input>
-  </div>
-}
 
+// for (let i = 0; i < 111; i++) {
+//   document.getElementsByClassName("upgradeSimulator_button__upgrade__hJct8")[0].click();
+//   await new Promise((resolve) => setTimeout(resolve, 10));
+//   document.getElementsByClassName("upgradeSimulator_button__retry__SJlrV")[0].click();
+//   await new Promise((resolve) => setTimeout(resolve, 10));
+// }
