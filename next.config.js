7280efa8d489ac232754c162e5951f311571ffb0
Arat5724 <arat5724@gmail.com>
2024-07-09 08:49:58 +0900

    change timeout

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  staticPageGenerationTimeout: 480,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  staticPageGenerationTimeout: 120,
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
 }
 
 module.exports = nextConfig
