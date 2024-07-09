a181f5d42ef2aaed9398c0106123c80b9b54a0f8
Arat5724 <arat5724@gmail.com>
2024-07-09 09:22:10 +0900

    arrange

--- src/app/ui/components/navicon.tsx
+++ src/app/ui/components/navicon.tsx
@@ -22,6 +22,8 @@ export function NavIcon() {
       ></div>
     </div>
     <ul className={clsx(styles.ul, isNavOpen ? "" : styles.hidden)}>
+      <li className={styles["category"]}>시뮬레이터</li>
+      <li onClick={() => setIsNavOpen(false)}><LinkWrap href="/simulator/upgrade"><div className={styles["link-item"]}>강화 시뮬레이터</div></LinkWrap></li>
       <li className={styles["category"]}>현질 효율</li>
       <li onClick={() => setIsNavOpen(false)}><LinkWrap href="/general-product"><div className={styles["link-item"]}>일반 상품</div></LinkWrap></li>
       <li onClick={() => setIsNavOpen(false)}><LinkWrap href="/mileage-product"><div className={styles["link-item"]}>마일리지 상품</div></LinkWrap></li>
@@ -30,8 +32,6 @@ export function NavIcon() {
       <li onClick={() => setIsNavOpen(false)}><LinkWrap href="/box"><div className={styles["link-item"]}>상자</div></LinkWrap></li>
       <li onClick={() => setIsNavOpen(false)}><LinkWrap href="/bp"><div className={styles["link-item"]}>BP 카드</div></LinkWrap></li>
       <li onClick={() => setIsNavOpen(false)}><LinkWrap href="/cp"><div className={styles["link-item"]}>CP 카드</div></LinkWrap></li>
-      <li className={styles["category"]}>시뮬레이터</li>
-      <li onClick={() => setIsNavOpen(false)}><LinkWrap href="/simulator/upgrade"><div className={styles["link-item"]}>강화 시뮬레이터</div></LinkWrap></li>
       <li className={styles["category"]}>기타</li>
       <li onClick={() => setIsNavOpen(false)}><LinkWrap href="/board"><div className={styles["link-item"]}>자유 게시판</div></LinkWrap></li>
     </ul>
