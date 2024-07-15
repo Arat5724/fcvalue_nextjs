1fbda479ee4f4df3e6e90466b56f37525f474bf6
Arat5724 <arat5724@gmail.com>
2024-07-15 21:22:09 +0900

    fix big tots

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
   return <>
     <div>
       <h2>패치 노트</h2>
+      <p>- <a href="/box/11002">[큰 토츠 60%] 큰 토츠 or 작은 토츠 상자</a>의 기댓값이 수정되었습니다.</p>
       <p>- <a href="/general-product/44209">[유로 더블 위크] 24TOTS, EU24 8강 포함 상자</a> 상품 추가 완료</p>
       <p>- 나혼자만 럭키! SSS 추가 완료.</p>
       {/* <p>- 20240627 업데이트 반영 완료.</p> */}
