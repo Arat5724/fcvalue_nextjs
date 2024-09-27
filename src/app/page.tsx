faab8a497cf9201d8019fc57f083f7fd90c04cf0
Arat5724 <arat5724@gmail.com>
2024-09-27 18:20:57 +0900

    240927

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
       {/* <h2>공지 사항</h2> */}
       {/* <p>- 집중 훈련 패스는 환급되는 FC만큼 할인된 금액으로 기댓값 및 효율이 계산되었습니다.</p> */}
       <h2>패치 노트</h2>
+      <p>- 240927 24EP 클래스, 상품 추가.</p>
       <p>- 240919 배틀패스 추가.</p>
-      <p>- 추석 패키지 업데이트.</p>
       <h2>시뮬레이터</h2>
       <GridWrap>
         <GridItem
@@ -28,12 +28,12 @@ export default function Home() {
       <h2>현질 효율</h2>
       <GridWrap>
         <GridItem
-          img="/assets/image/item/14255_s.png"
+          img="/assets/image/item/14262_s.png"
           href="/general-product"
           title="일반 상품"
         />
         <GridItem
-          img="/assets/image/item/31297_s.png"
+          img="/assets/image/item/31332_s.png"
           href="/mileage-product"
           title="마일리지 상품"
         />
@@ -41,12 +41,12 @@ export default function Home() {
       <h2>아이템 정보</h2>
       <GridWrap>
         <GridItem
-          img="/assets/image/item/200230720_s.png"
+          img="/assets/image/item/200237136_s.png"
           href="/player-pack"
           title="선수팩"
         />
         <GridItem
-          img="/assets/image/item/201706023_s.png"
+          img="/assets/image/item/201706246_s.png"
           href="/box"
           title="상자"
         />
